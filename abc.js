73bd39ed30810f286e64dd9fffc2ad2c52cf013b
Aditya Yaji <56244380+adityayaji@users.noreply.github.com>
2021-09-19 15:59:24 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,16 @@
+function toggleText(argument) {
+	var points = document.getElementById("points");
+	var showMoreText = document.getElementById("moreText");
+	var buttonText=document.getElementById("textButton");
+	if(points.style.display !="none"){
+		showMoreText.style.display="none";
+		points.style.display = "none";
+		buttonText.innerHTML = "Show More";
+	}
+	else {
+		showMoreText.style.display= "inline";
+		points.style.display= "inline";
+		buttonText.innerHTML = "Show Less";
+	}
+}
+
